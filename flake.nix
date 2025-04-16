b37517fdae573882e5b017cb9f43932b79f0da9d
Rushabh Patil <rushabhpatil@tutamail.com>
2025-04-16 11:27:51 +0530

    modularized config ig --> WIP

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
+          ./hosts/laptop/configuration.nix
         ];
       };
     };
